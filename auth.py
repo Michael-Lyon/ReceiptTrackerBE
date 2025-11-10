cedcb251164a19c888a5a583894549080bf389b4
Michael Asomugha <mickiasomg@gmail>
2025-11-10 09:38:30 +0100

    Updated access token time

--- auth.py
+++ auth.py
@@ -7,7 +7,7 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key-change-in-production")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 30000
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/login")
 
